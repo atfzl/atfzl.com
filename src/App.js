a2064fba43296f21f68801eb403acd5cf8353102
Atif Afzal <atif5801@gmail.com>
2018-11-28 15:31:11 +0530

    Use Ibm plex from google fonts

--- src/App.js
+++ src/App.js
@@ -4,12 +4,11 @@ import { Router, Link } from 'react-static';
 import Routes from 'react-static-routes';
 
 import 'normalize.css';
-import '@ibm/type/css/ibm-type.min.css';
 import './app.css';
 
 export default () => (
   <Router>
-    <div className="ibm">
+    <div>
       <div className="header">
         <Link className="header-link" to="/">
           <img className="home-icon" src="/images/home-icon.png" alt="home" />
